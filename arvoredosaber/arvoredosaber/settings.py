74812ad2717e7e4ad0f3920f463328c7da53e5d8
Paloma C. S. Soares <paloma.sew@gmail.com>
2022-06-08 15:03:12 -0300

    criação de função para posts

--- arvoredosaber/arvoredosaber/settings.py
+++ arvoredosaber/arvoredosaber/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reviews',
 ]
 
 MIDDLEWARE = [
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- arvoredosaber/blog/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Cadastro(models.Model):
-    nome = models.CharField(max_length=100)
-    email = models.CharField(max_length=150)
